505964306417df7c889673e391047d288ea8e430
SlidersInsider <maks.zhadan00@mail.ru>
2023-04-13 16:15:57 +0300

    init commit

---
+++ src/main/kotlin/com/mzhadan/phoneaccountingserver/PhoneAccountingServerApplication.kt
@@ -0,0 +1,11 @@
+package com.mzhadan.phoneaccountingserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PhoneAccountingServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<PhoneAccountingServerApplication>(*args)
+}
